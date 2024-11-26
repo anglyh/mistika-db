6a45fc651065acf94f58ffa27d45be25a93db91c
Angel Yaguno <angel.yaguno@tecsup.edu.pe>
2024-11-25 19:49:17 -0500

    listen all

--- index.js
+++ index.js
@@ -23,4 +23,5 @@ app.use('/events', eventRoutes);
 app.use('/places', placeRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
